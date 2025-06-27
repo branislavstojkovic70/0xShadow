6df8044fd1f733408750d9323fd7350b850c41f8
lukaDjordjevic01 <lukadjordjevicsm01@gmail.com>
2025-06-27 19:38:17 +0200

    Added seed form for wallet creation.

--- frontend/src/pages/seed.tsx
+++ frontend/src/pages/seed.tsx
@@ -1,7 +1,15 @@
-import { Container } from "@mui/material";
+import { Container, Divider, Typography } from "@mui/material";
+import SeedForm from "../components/seed-form";
 
 export default function Seed() {
-	return(
-		<Container>Hello from Seed</Container>
-	)
+	return (
+		<Container maxWidth="sm" sx={{ mt: 5 }}>
+			<Typography variant="h4" fontWeight={600} textAlign="center" sx={{mb: 2}}>
+				Generate wallet
+			</Typography>
+
+			<Divider />
+			<SeedForm />
+		</Container>
+	);
 }
